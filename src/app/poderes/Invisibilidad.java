b46fe612c3add5d2231425f21b1285e694579866
alexmarygr <alexmarygr@gmail.com>
2020-09-04 13:31:52 -0300

    git init

--- src/app/poderes/Invisibilidad.java
+++ src/app/poderes/Invisibilidad.java
@@ -1,7 +1,6 @@
 package app.poderes;
 
 public class Invisibilidad extends Poder {
-    // Constructor de Invisibilidad
     public Invisibilidad(String nombrePoder, String descripcionPoder) {
         super(nombrePoder, descripcionPoder);
     }
